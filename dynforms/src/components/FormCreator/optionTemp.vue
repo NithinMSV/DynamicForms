6e32c55b2be1eff039a616b3a858dac74e6390ad
nithinmsv <nithinmsv@gmail.com>
2020-11-16 11:53:06 +0530

    Done!

---
+++ dynforms/src/components/FormCreator/optionTemp.vue
@@ -0,0 +1,17 @@
+<template>
+    <b-form-radio v-model="selected" name="some-radios" :value="value"><b-form-input v-model="text" placeholder="Enter the option"><slot /></b-form-input>
+    </b-form-radio>
+</template>
+
+<script>
+export default {
+    name: 'OptionTemp',
+    props: ['value'],
+    data(){
+        return {
+            selected: '',
+            text: ''
+        }
+    }
+}
+</script>
